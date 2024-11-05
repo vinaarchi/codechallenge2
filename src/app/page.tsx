0845e7e158745b5b2d76cf7e4296bbe246459f54
vinaarchi <vinaa.as4@gmail.com>
2024-11-05 10:36:31 +0700

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import About from "@/sections/Aboutus";
 import Testi from "@/sections/testimonials";
 import Prodpro from "@/components/Prodpro";
 import Team from "@/sections/Team";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
