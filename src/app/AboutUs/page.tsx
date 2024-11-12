4a70ffb13bc2023a9d83d85a3f7f75bcebf626c1
vinaarchi <vinaa.as4@gmail.com>
2024-11-13 00:07:47 +0700

    message

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
@@ -14,11 +14,14 @@ const AboutUs = () => {
         <h1 className="font-rafeny text-4xl text-customDarkBlue pb-6">
           About Us
         </h1>
-        <img
+        <Image
           src="/images/AboutUs.webp"
-          className="w-96 rounded-md shadow-md mb-6"
+          alt="About Us"
+          width={500}
+          height={300}
+          className="rounded-md"
         />
-        <p className="text-center mb-6">
+        <p className="text-center mb-6 mt-6">
           Selamat datang di Minha Nail Art, tempat di mana kreativitas bertemu
           dengan presisi dalam seni merias kuku. Didirikan pada tahun 2024
           dengan misi menghadirkan seni kuku berkualitas tinggi yang memukau,
@@ -33,11 +36,14 @@ const AboutUs = () => {
         <h1 className="font-rafeny text-4xl text-customDarkBlue pb-6">
           Team Kami
         </h1>
-        <img
+        <Image
           src="/images/Our Team.webp"
-          className="w-96 rounded-md shadow-md mb-6"
+          alt="Our Team"
+          width={500}
+          height={300}
+          className="rounded-md"
         />
-        <p className="text-center mb-6">
+        <p className="text-center mt-6 mb-6">
           Tim kami terdiri dari para seniman dan teknisi kuku yang terampil,
           masing-masing dengan keahlian unik dan dedikasi tinggi terhadap
           kesempurnaan. Kami percaya dalam pembelajaran berkelanjutan, dan para
@@ -46,14 +52,17 @@ const AboutUs = () => {
         </p>
       </div>
       <div className="flex flex-col justify-center items-center p-7">
-        <h1 className="font-rafeny text-4xl text-customDarkBlue pt-4pb-6">
+        <h1 className="font-rafeny text-4xl text-customDarkBlue pt-4 pb-6">
           Budaya
         </h1>
-        <img
+        <Image
           src="/images/culture.webp"
-          className="w-96 rounded-md shadow-md mb-6"
+          alt="culture"
+          width={500}
+          height={300}
+          className="rounded-md"
         />
-        <p className="text-center">
+        <p className="text-center mt-6">
           Di Minha Nail Art, kami menjunjung tinggi budaya kreativitas,
           inklusivitas, dan keunggulan. Kami merayakan keunikan setiap individu
           dan percaya bahwa seni kuku adalah cara yang kuat untuk
