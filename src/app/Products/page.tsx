402b6fb9821a8ec46df6a4f739193ed6c09da7f6
vinaarchi <vinaa.as4@gmail.com>
2024-11-14 17:32:26 +0700

    message

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,29 +1,23 @@
-// import Product from "@/views/pages/home";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-
-// export default function Products() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div
-//         className="flex flex-col justify-center items-center"
-//         style={{ backgroundColor: "rgb(237, 218, 212)" }}
-//       >
-//         <p className="text-center p-6 font-helvetica w-[70%]">
-//           Yuk, lihat koleksi produk dan program kami dan temukan yang cocok
-//           untukmu! Kualitas terbaik dengan desain menarik hanya untuk kamu.
-//         </p>
-//       </div>
-//       <Product />
-
-//       <Footer />
-//     </div>
-//   );
-// }
+import Product from "@/views/pages/home";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Products() {
   return (
-    <div>contoh</div>
-  )
+    <div>
+      <Navbar />
+      <div
+        className="flex flex-col justify-center items-center"
+        style={{ backgroundColor: "rgb(237, 218, 212)" }}
+      >
+        <p className="text-center p-6 font-helvetica w-[70%]">
+          Yuk, lihat koleksi produk dan program kami dan temukan yang cocok
+          untukmu! Kualitas terbaik dengan desain menarik hanya untuk kamu.
+        </p>
+      </div>
+      <Product />
+
+      <Footer />
+    </div>
+  );
 }
--- src/lib/contentfulClient.ts
+++ src/lib/contentfulClient.ts
@@ -1,15 +1,15 @@
-// import { createClient } from "contentful";
+import { createClient } from "contentful";
 
-// const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-// const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 
-// if (!spaceId || !accessToken) {
-//   console.error("Contentful space ID or access token is missing!");
-// }
+if (!spaceId || !accessToken) {
+  console.error("Contentful space ID or access token is missing!");
+}
 
-// const contentfulClient = createClient({
-//   space: spaceId || "",
-//   accessToken: accessToken || "",
-// });
+const contentfulClient = createClient({
+  space: spaceId || "",
+  accessToken: accessToken || "",
+});
 
-// export default contentfulClient;
+export default contentfulClient;
