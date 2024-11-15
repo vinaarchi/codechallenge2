1ab5b13c268fe7c02b0b59895f28fa1c6b73339c
vinaarchi <vinaa.as4@gmail.com>
2024-11-15 13:00:48 +0700

    message

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,28 +1,25 @@
 "use client";
 
-import * as React from "react"
+import * as React from "react";
 import Image from "next/legacy/image";
 
-
 const Hero = () => {
-
   return (
     <div className="relative w-full h-screen bg-cover bg-center">
-
-      <Image 
-      src="/images/1bg.webp"
-      alt="Background"
-      layout="fill"
-      objectFit="cover"
-      priority={true}
+      <Image
+        src="/images/1bg.webp"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        priority={true}
       />
       <div className="absolute top-[5vh] flex flex-col justify-center items-center w-full h-full text-white">
         <div className="font-rafeny text-5xl font-semibold text-center">
           <h1>Minha Nail Art</h1>
-          <h3>Creating Beauty</h3>
+          <p>Creating Beauty</p>
         </div>
         <div className="font-helvetica italic">
-          <h5>Sentuhan Elegan untuk Setiap Kesempatan</h5>
+          <p>Sentuhan Elegan untuk Setiap Kesempatan</p>
         </div>
       </div>
     </div>
