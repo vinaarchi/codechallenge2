e8ad57ddbe7aef53a0d7a794abfd19d5f4e74bc9
vinaarchi <vinaa.as4@gmail.com>
2024-11-21 23:32:07 +0700

    message

--- src/views/pages/home/index.tsx
+++ src/views/pages/home/index.tsx
@@ -1,17 +1,16 @@
 import contentfulClient from "@/lib/contentfulClient";
 import { TypeProductSkeleton, TypeProductAsset } from "@/types/product.type";
-import Image from "next/image";
+
 
 const getProducts = async () => {
   try {
     const product = await contentfulClient.getEntries<TypeProductSkeleton>({
       content_type: "product",
     });
-    console.log(product.items[0].fields.image);
-
     return product;
   } catch (error) {
-    console.log(error);
+    console.error("Failed to fetch products:", error);
+    return { items: [] }; // Default value
   }
 };
 
@@ -30,27 +29,27 @@ export default async function Product({ isLandingPage = false }: ProductProps) {
           : "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3"
       }`}
     >
-      {product &&
-        product.items?.map((product, index) => (
-          <div key={index} className="flex flex-col items-center">
-            <p className="font-rafeny text-customPink text-2xl">
-              {product.fields.title}
-            </p>
-            <div className="w-64 h-auto overflow-hidden">
-              <Image
-                src={`https:${
-                  (product.fields.image as TypeProductAsset)?.fields.file.url
-                }`}
-                alt={product.fields.title}
-                className="w-full h-auto rounded-md"
-              />
-            </div>
-            <p className="font-helvetica text-customDarkBlue mb-10">
-              {" "}
-              Rp {product.fields.price}
-            </p>
+      {product.items?.map((product) => (
+        <div key={product.sys.id} className="flex flex-col items-center">
+          <p className="font-rafeny text-customPink text-2xl">
+            {product.fields.title}
+          </p>
+          <div className="w-64 h-auto overflow-hidden">
+            <img
+              src={`https:${
+                (product.fields.image as TypeProductAsset)?.fields.file.url
+              }`}
+              alt={product.fields.title}
+              width={256}
+              height={256}
+              className="w-full h-auto rounded-md"
+            />
           </div>
-        ))}
+          <p className="font-helvetica text-customDarkBlue mb-10">
+            Rp {product.fields.price}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
